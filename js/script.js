503a8a19f2f87a892761ea5b25ca78e51a0a5a04
kevin_p <kprsd14@gmail.com>
2021-08-08 22:04:27 -0400

    Added repo fetch capability

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Profile Information Element
-const overview = document.querySelector('.overview');
+const overview = document.querySelector('.overview'),
+      repoList = document.querySelector('.repo-list');
 
 // GitHub Username
 const username = 'kpersaud-dev';
@@ -7,19 +8,22 @@ const username = 'kpersaud-dev';
 // Fetch GitHub Profile
 
 const fetchProfile = async () => {
+  // Fetch username data
   const res = await fetch(`https://api.github.com/users/${username}`);
+  //Convert to JSON
   const data = await res.json();
   console.log(data);
 
   // Call Display Info Function
   displayInfo(data);
+
 }
 
 fetchProfile();
 
 // Display User Information
 
-const displayInfo = async (jsonData) => {
+const displayInfo = async jsonData => {
   // Create div for user info
   const userInfo = document.createElement('div');
   //Populate user info div with data
@@ -37,4 +41,27 @@ const displayInfo = async (jsonData) => {
   // Append Div
   overview.append(userInfo);
 }
+
+// Fetch Repos
+
+const fetchRepos = async () => {
+  // Sort by recently updated and display 100 per page
+  const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  // Convert to JSON
+  const repoData = await res.json();
+  displayRepos(repoData);
+}
+
+// Display Repo Information
+
+const displayRepos = repos => {
+  for(let repo of repos) {
+    const li = document.createElement('li');
+    li.classList.add('repo');
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(li);
+  }
+}
+
+fetchRepos();
 
